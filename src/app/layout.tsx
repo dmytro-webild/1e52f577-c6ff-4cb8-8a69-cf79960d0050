b7fd46475b1a7c990b693ba6bf53bb733c2b1c80
dk <kudin.dima@gmail.com>
2026-04-23 17:07:41 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Mulish } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Olivia Lallouz | Orofacial Myologist & Wellness Specialist',
+  description: 'Transform your bite, breathe better, and sleep deeper with specialized orofacial myofunctional therapy sessions and wellness courses by Olivia Lallouz.',
+};
+
+const mulish = Mulish({ variable: "--font-mulish", subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${mulish.variable} antialiased`}>
+          
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
